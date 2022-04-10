221fd0e0b54106449a9320fb5e619610d769d138
yasseralkabbani <yasseralkabbani1411989@gmail.com>
2022-04-10 13:09:44 +0300

    Support Share File

--- core-manager/src/main/java/com/yasser/coremanager/manager/StartActivityManager.kt
+++ core-manager/src/main/java/com/yasser/coremanager/manager/StartActivityManager.kt
@@ -1,10 +1,12 @@
 package com.yasser.coremanager.manager
 
 import android.content.Intent
+import java.io.File
 
 sealed class StartActivityManager {
     object GoToSettings:StartActivityManager()
     class GoToSendEmail(val emailAddress:String,val subject:String,val body:String):StartActivityManager()
     class StartCallPhone(val phoneNumber:String):StartActivityManager()
+    class ShareFile(val file:File):StartActivityManager()
     class CustomIntent(val intent:Intent):StartActivityManager()
 }
